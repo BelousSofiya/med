46a6a9b18a6076a3cbd76545dcb02ceca4451a57
Sofia Bilous <EgikSonya@gmail.com>
2023-01-18 18:30:53 +0200

    Medical triage

---
+++ Startadulteng1.py
@@ -0,0 +1,18 @@
+stand = "Can the patient walk? print '+' or '-'"
+breath = "Does the patient have spontaneous breathing? write'+' or '-'"
+breath2 ="restore airway patency. Does the patient have spontaneous breathing now? write'+' or '-'"
+age = "Is the patint a child 6 years or early? write'+' or '-'"
+breathing_rate = "Is breathing rate more then 30 in 1 minut? For children: is breathing rate less then 15 or more then 45? write'+' or '-'"
+pulse = "Does the patient have pulse on radial artery? write'+' or '-'"
+conscious = "Is he conscious? write'+' or '-'"
+g = "green"
+r = "red"
+y = "yellow"
+b = "black"
+    
+
+
+    
+    
+        
+
