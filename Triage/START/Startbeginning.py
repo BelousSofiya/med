16c81276fd5d7099e1ab74ec7b1a72df9d38063c
Sofia Bilous <EgikSonya@gmail.com>
2023-01-18 19:19:58 +0200

    create salt

---
+++ Triage/START/Startbeginning.py
@@ -0,0 +1,57 @@
+choice = input("What language would you prefer? Write 'eng', 'ru', 'ukr'")
+if choice == "eng":
+    from Startadulteng1 import *  
+elif choice == "ru":
+    from Startadultru1 import *
+else:
+    from Startadultukr1 import *
+    
+stand1=(input(stand))
+if stand1 == "+":
+    print(g)
+    input
+else:
+    airways1=(input(airways))
+    if airways1 =="-":
+        breath1=(input(breath))
+        if breath1 == "-":
+            age1=input(age)
+            if age1 =="+":
+                breath31 = input(breath3)
+                if breath31 == "-":
+                    print(b)
+                    input
+                else:
+                    print(r)
+                    input
+            else:
+                print(b)
+                input
+        else:
+            print(r)
+            input
+    else:
+        breathing_rate1 = input(breathing_rate)
+        if breathing_rate1 == "+":
+            print(r)
+            input
+        else:
+            pulse1 = input(pulse)
+            if pulse1 == "-":
+                print(r)
+                input
+            else:
+                conscious1 = input(conscious)
+                if conscious1 == "+":
+                    print(y)
+                    input
+                else:
+                    print(r)
+                    input()
+    
+    
+
+    
+
+        
+
